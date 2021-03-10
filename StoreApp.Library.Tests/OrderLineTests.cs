ba1d1f924ac3894af51dc6722a4c1af9fd89691d
Bryson <brysonewell@outlook.com>
2021-03-10 13:33:59 -0500

    Added OrderLine LineTotal test.

---
+++ StoreApp.Library.Tests/OrderLineTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using StoreApp.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StoreApp.Library.Tests
+{
+    public class OrderLineTests
+    {
+        [Fact]
+        public void OrderLine_LineTotal()
+        {
+            // arrange
+            const decimal PRODUCT_PRICE = 1.29m;
+            const int QUANTITY = 4;
+            var product = new Mock<IProduct>();
+            product.Setup(p => p.UnitPrice).Returns(PRODUCT_PRICE);
+            IOrderLine orderLine = new OrderLine(product.Object, QUANTITY);
+
+            // act
+            decimal totalPrice = orderLine.LineTotalPrice;
+
+            // assert
+            Assert.Equal(PRODUCT_PRICE * QUANTITY, totalPrice, 3);
+        }
+    }
+}
